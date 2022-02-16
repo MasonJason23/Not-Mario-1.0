bf75934aae83c4b4c14ecc2393480e1aaf92ebc4
Jason Casareno <jcasareno@csumb.edu>
2022-02-15 20:05:42 -0800

    Initial commit

---
+++ Assets/Scripts/LevelParserStarter.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LevelParserStarter : MonoBehaviour
+{
+    public string filename;
+
+    public GameObject Rock;
+
+    public GameObject Brick;
+
+    public GameObject QuestionBox;
+
+    public GameObject Stone;
+
+    public Transform parentTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshParse();
+    }
+
+
+    private void FileParser()
+    {
+        string fileToParse = string.Format("{0}{1}{2}.txt", Application.dataPath, "/Resources/", filename);
+
+        using (StreamReader sr = new StreamReader(fileToParse))
+        {
+            string line = "";
+            int row = 0;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                int column = 0;
+                char[] letters = line.ToCharArray();
+                foreach (var letter in letters)
+                {
+                    //Call SpawnPrefab
+                }
+
+            }
+
+            sr.Close();
+        }
+    }
+
+    private void SpawnPrefab(char spot, Vector3 positionToSpawn)
+    {
+        GameObject ToSpawn;
+
+        switch (spot)
+        {
+            case 'b': Debug.Log("Spawn Brick"); break;
+            case '?': Debug.Log("Spawn QuestionBox"); break;
+            case 'x': Debug.Log("Spawn Rock"); break;
+            case 's': Debug.Log("Spawn Rock"); break;
+            //default: Debug.Log("Default Entered"); break;
+            default: return;
+                //ToSpawn = //Brick;       break;
+        }
+
+        //ToSpawn = GameObject.Instantiate(ToSpawn, parentTransform);
+        //ToSpawn.transform.localPosition = positionToSpawn;
+    }
+
+    public void RefreshParse()
+    {
+        GameObject newParent = new GameObject();
+        newParent.name = "Environment";
+        newParent.transform.position = parentTransform.position;
+        newParent.transform.parent = this.transform;
+        
+        if (parentTransform) Destroy(parentTransform.gameObject);
+
+        parentTransform = newParent.transform;
+        FileParser();
+    }
+}
