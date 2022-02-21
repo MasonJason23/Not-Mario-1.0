79d8f1697c38d4f03210f1168c2e86e9728ec8a5
Jason Casareno <jcasareno@csumb.edu>
2022-02-20 18:43:44 -0800

    Created a script that casts a ray from the main camera to the mouse position in the game view.

--- Assets/Scripts/MouseRaycast.cs
+++ Assets/Scripts/MouseRaycast.cs
@@ -1,18 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseRaycast : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         
+        if (Physics.Raycast(mouseRay, out RaycastHit hit))
+        {
+            Debug.Log("Hit!");
+        }
     }
 }
