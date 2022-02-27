3de1cd2291a409113e8e29382fb6c13a1f389ed4
Jason Casareno <jcasareno@csumb.edu>
2022-02-26 23:22:37 -0800

    Updated raycast to be smaller as it allowed an late extended jump when getting closer to the ground Updated rigidbody collision so there is no stutter-like movement when running and falling back to the ground

--- Assets/Scripts/MainCharacterController.cs
+++ Assets/Scripts/MainCharacterController.cs
@@ -11,7 +11,15 @@ public class MainCharacterController : MonoBehaviour
     public float longJumpForce = 3f;
     public bool grounded;
     public bool rotated;
+
+    // Coyote Time Variables
+    private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
     
+    // Jump Buffer Variables
+    private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
+
     private Rigidbody rbody;
     private Collider collider;
     private Animator animComp;
@@ -29,14 +37,14 @@ public class MainCharacterController : MonoBehaviour
     void Update()
     {
         // Checking to make sure the player is on the ground in order to jump
-        float castDistance = collider.bounds.extents.y + 0.1f;
+        float castDistance = collider.bounds.extents.y * 0.1f;
         grounded = Physics.Raycast(transform.position, Vector3.down, castDistance);
-        Debug.DrawRay(transform.position, Vector3.down * castDistance);
-        
+
         // Move left or right by applying force
         float axis = Input.GetAxis("Horizontal");
         rbody.AddForce(Vector3.right * axis * runForce, ForceMode.Force);
 
+        // Rotates player to look left or right based on arrow keys pressed
         if (Input.GetKey(KeyCode.LeftArrow) && rbody.velocity.magnitude > 0.1f && !rotated)
         {
             rotated = true;
@@ -48,6 +56,7 @@ public class MainCharacterController : MonoBehaviour
             transform.Rotate(0f, 180f, 0f);
         }
 
+        // Updates animator controller parameters to do animations when set parameter meets requirements
         if (rbody.velocity.magnitude > 0.1f)
         {
             animComp.SetBool("Moving", true);
@@ -56,16 +65,41 @@ public class MainCharacterController : MonoBehaviour
         {
             animComp.SetBool("Moving", false);
         }
+        
+        // Updating coyote time variables
+        if (grounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+        
+        // Updating jump buffer variables
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
 
-        // Jump with two parameters (character is on the ground and space bar is pressed)
-        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        // Jump. Using coyote time and jump buffering + extended jump when space is held
+        if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f)
         {
             rbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            jumpBufferCounter = 0f;
         }
         else if (rbody.velocity.y > 0f && Input.GetKey(KeyCode.Space))
         {
             rbody.AddForce(Vector3.up * longJumpForce, ForceMode.Force);
+
+            coyoteTimeCounter = 0f;
         }
+        
         
         // Capping the character's speed limit
         if (Mathf.Abs(rbody.velocity.x) > maxRunSpeed)
